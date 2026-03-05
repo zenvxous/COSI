8593ae8727049e050dc6bf06b050cbc9c6b1ad5f
shkafenko1 <klucenkovegor2@gmail.com>
2026-03-05 17:10:40 +0300

    lab2 fix

--- lab2/main.py
+++ lab2/main.py
@@ -3,49 +3,39 @@ from scipy.io import wavfile
 from scipy.signal import lfilter, firwin, butter, filtfilt
 import matplotlib.pyplot as plt
 
-# Ввод длины сигнала N (степень двойки)
 N = int(input("Введите длину сигнала N (степень двойки): "))
 if (N & (N - 1)) != 0:
     raise ValueError("Ошибка: N не является степенью двойки!")
 
-fs = 8000  # Частота дискретизации (Гц)
-t = np.arange(N) / fs  # Временная ось
+fs = 8000
+t = np.arange(N) / fs
 
-# 1. Генерация синтетического сигнала (струнного типа)
-f0 = 220  # Основная частота 220 Гц
+f0 = 220
 signal = np.zeros(N)
-# Суммируем первые 5 гармоник с убывающими амплитудами
 amplitudes = [1.0, 0.5, 0.3, 0.2, 0.1]
 for k, amp in enumerate(amplitudes, start=1):
     signal += amp * np.sin(2 * np.pi * f0 * k * t)
-# Добавляем белый шум небольшого уровня
 signal += 0.05 * np.random.randn(N)
 
-# 2. Применение фильтров
-
-# 2.1. Однородный скользящий средний фильтр (M = 16)
 M = 16
 b_mavg = np.ones(M) / M
 a_mavg = 1
 signal_mavg = lfilter(b_mavg, a_mavg, signal)
 
-# 2.2. НЧ FIR-фильтр (Hamming, M = 101, fc = 300 Гц)
-fc = 300  # частота среза 300 Гц
+fc = 300
 M_fir = 101
 b_fir = firwin(M_fir, cutoff=fc, window='hamming', fs=fs)
 a_fir = 1
 signal_lowpass = lfilter(b_fir, a_fir, signal)
 
-# 2.3. Полосовой IIR-фильтр (Butterworth, f0 = 275 Гц, полоса = 250 Гц)
-f_center = 275  # центральная частота
-BW = 250  # ширина полосы
-low_freq = f_center - BW/2  # нижняя граничная частота
-high_freq = f_center + BW/2 # верхняя граничная частота
-order = 4  # порядок фильтра
+f_center = 275
+BW = 250
+low_freq = f_center - BW/2
+high_freq = f_center + BW/2
+order = 4
 b_iir, a_iir = butter(order, [low_freq, high_freq], btype='bandpass', fs=fs)
-signal_bandpass = filtfilt(b_iir, a_iir, signal)  # zero-phase фильтрация
+signal_bandpass = filtfilt(b_iir, a_iir, signal)
 
-# 3. Сохранение отфильтрованных сигналов в WAV-файлы (номиналируем к int16)
 wavfile.write("output_moving_average.wav", fs, 
               np.int16(signal_mavg/np.max(np.abs(signal_mavg)) * 32767))
 wavfile.write("output_lowpass_fir.wav", fs, 
@@ -53,9 +43,6 @@ wavfile.write("output_lowpass_fir.wav", fs,
 wavfile.write("output_bandpass_iir.wav", fs, 
               np.int16(signal_bandpass/np.max(np.abs(signal_bandpass)) * 32767))
 
-# 4. Построение графиков
-
-# 4.1. Временные области сигналов
 plt.figure(figsize=(10, 8))
 plt.subplot(4, 1, 1)
 plt.plot(t, signal)
@@ -89,7 +76,6 @@ plt.tight_layout()
 plt.savefig("waveform_plot.png")
 plt.close()
 
-# 4.2. Амплитудные спектры сигналов
 def plot_spectrum(x, fs, title, ax):
     N = len(x)
     X = np.fft.rfft(x)
